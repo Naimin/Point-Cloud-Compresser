1b9f1b631dd75b7320caf16e9167ab771d02eba2
ADS\naimink <koh.naimin@autodesk.com>
2018-10-30 14:31:18 +0800

    Fix the return type of the morton encode64().

--- PointCloudCompresser/src/MortonCode.cpp
+++ PointCloudCompresser/src/MortonCode.cpp
@@ -8,7 +8,7 @@ unsigned int CPC::MortonCode::encode32(const Index & index)
     return libmorton::morton3D_32_encode(index.index.x(), index.index.y(), index.index.z());
 }
 
-unsigned int CPC::MortonCode::encode64(const Index & index)
+unsigned long long CPC::MortonCode::encode64(const Index & index)
 {
     return libmorton::morton3D_64_encode(index.index.x(), index.index.y(), index.index.z());
 }
--- PointCloudCompresser/src/MortonCode.h
+++ PointCloudCompresser/src/MortonCode.h
@@ -8,7 +8,7 @@ namespace CPC
     {
         public:
             static unsigned int encode32(const Index & index);
-            static unsigned int encode64(const Index & index);
+            static unsigned long long encode64(const Index & index);
             static Index decode32(const unsigned int code);
             static Index decode64(const unsigned long long code);    
     };
